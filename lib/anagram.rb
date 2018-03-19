d0fcfd8b5e6be24a96077a3584bfc7ffee66aead
azeelleeza <leezaluncheon@gmail.com>
2018-03-19 10:59:33 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(string)
+    @string = string
+  end
+
+  def match(array)
+    array.select do |element|
+      element.chars.sort == @string.chars.sort
+    end
+  end
+
+end
