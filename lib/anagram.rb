76fc0a560aa5bfe5d3c691e9c7b30b6c06805d07
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-03-20 10:05:12 -0400

    lab completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,41 @@
 # Your code goes here!
+# Your class, Anagram should take a word on initialization,
+# and instances should respond to a match method that takes
+# an array of possible anagrams. It should return all matches
+# in an array. If no matches exist, it should return an empty array.
+require "pry"
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+
+  def sorted_word
+    word.split("").sort
+  end
+
+  def match(string_array)
+    answer = []
+
+    all_words_stringed(string_array).each_with_index { |x, i|
+      if x == sorted_word
+        answer.push(string_array[i])
+      end
+    }
+    # binding.pry
+    # answer = []
+    if !!answer[0]
+      answer
+    else
+      []
+    end
+  end
+
+  def all_words_stringed(string_array)
+    all_words_stringed = []
+    string_array.each do |word|
+      all_words_stringed.push(word.split("").sort)
+    end
+    all_words_stringed
+  end
+end
